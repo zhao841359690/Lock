535d0f5780d52baa78fcdb9a2080d535ac4d4b60
zhaoqingfeng <bguo@xumo.com>
2019-10-28 09:04:47 +0800

    添加引导页面

--- app/src/main/java/com/zhao/lock/SplashActivity.java
+++ app/src/main/java/com/zhao/lock/SplashActivity.java
@@ -5,12 +5,19 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.gyf.immersionbar.ImmersionBar;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImmersionBar.with(this)
+                .statusBarView(findViewById(R.id.status_bar_view))
+                .keyboardEnable(true)
+                .init();
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
