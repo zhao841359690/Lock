6e4f5dc6162a4d8cd05a08a940f0b9d4669a8e48
kk-zhaoqingfeng <klin@xumo.com>
2020-03-26 14:32:56 +0800

    代码修改

--- app/src/main/java/com/zhao/lock/ui/fragment/HomePageFragment.java
+++ app/src/main/java/com/zhao/lock/ui/fragment/HomePageFragment.java
@@ -1,5 +1,6 @@
 package com.zhao.lock.ui.fragment;
 
+import android.support.v7.widget.CardView;
 import android.text.Html;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -17,8 +18,8 @@ public class HomePageFragment extends BaseFragment {
     LinearLayout scanCodeLy;
     @BindView(R.id.ble_scan_ly)
     LinearLayout bleScanLy;
-    @BindView(R.id.pending_ly)
-    LinearLayout pendingLy;
+    @BindView(R.id.pending_cv)
+    CardView pendingCv;
     @BindView(R.id.pending_tv)
     TextView pendingTv;
     @BindView(R.id.ticket_number_tv)
@@ -53,7 +54,7 @@ public class HomePageFragment extends BaseFragment {
         pendingTv.setText(Html.fromHtml("您有一条<font color='#0E5EAB'>[待操作]</font>的订单"));
     }
 
-    @OnClick({R.id.scan_code_ly, R.id.ble_scan_ly, R.id.pending_ly})
+    @OnClick({R.id.scan_code_ly, R.id.ble_scan_ly, R.id.pending_cv})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.scan_code_ly:
@@ -62,7 +63,7 @@ public class HomePageFragment extends BaseFragment {
             case R.id.ble_scan_ly:
                 onHomePageFragmentClickListener.onBleScanClick();
                 break;
-            case R.id.pending_ly:
+            case R.id.pending_cv:
                 onHomePageFragmentClickListener.onPendingClick();
                 break;
             default:
