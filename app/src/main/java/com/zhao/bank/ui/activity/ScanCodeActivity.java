7af1228b9bb020252f49869c530b3cc3ea5b4eba
kk-zhaoqingfeng <klin@xumo.com>
2020-04-27 15:27:50 +0800

    添加扫码逻辑

--- app/src/main/java/com/zhao/bank/ui/activity/ScanCodeActivity.java
+++ app/src/main/java/com/zhao/bank/ui/activity/ScanCodeActivity.java
@@ -1,20 +1,31 @@
 package com.zhao.bank.ui.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.king.zxing.CaptureHelper;
 import com.king.zxing.OnCaptureCallback;
 import com.king.zxing.ViewfinderView;
 import com.zhao.bank.R;
+import com.zhao.bank.app.BaseApp;
 import com.zhao.bank.base.BaseActivity;
+import com.zhao.bank.bean.TodoOrdersBean;
 import com.zhao.bank.core.constant.Constants;
+import com.zhao.bank.util.SharedPreferencesUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import rxhttp.wrapper.param.RxHttp;
 
 public class ScanCodeActivity extends BaseActivity implements OnCaptureCallback {
     @BindView(R.id.title_left_iv)
@@ -30,6 +41,7 @@ public class ScanCodeActivity extends BaseActivity implements OnCaptureCallback 
     @BindView(R.id.ivTorch)
     View ivTorch;
 
+    private int fromWhere;
     private CaptureHelper mCaptureHelper;
 
     @Override
@@ -39,6 +51,8 @@ public class ScanCodeActivity extends BaseActivity implements OnCaptureCallback 
 
     @Override
     protected void initView() {
+        fromWhere = getIntent().getIntExtra("fromWhere", Constants.SCAN_CODE);
+
         titleLeftIv.setVisibility(View.VISIBLE);
         titleLineView.setVisibility(View.GONE);
 
@@ -77,9 +91,53 @@ public class ScanCodeActivity extends BaseActivity implements OnCaptureCallback 
 
     @Override
     public boolean onResultCallback(String result) {
-        Intent intent = new Intent();
-        intent.putExtra("result", result);
-        setResult(Constants.SCAN_CODE, intent);
-        return false;
+        if (fromWhere != Constants.SCAN_CODE) {
+            Intent intent = new Intent();
+            intent.putExtra("result", result);
+            setResult(fromWhere, intent);
+            return false;
+        } else {
+            RxHttp.get("/app/todoOrders")
+                    .add("token", SharedPreferencesUtils.getInstance().getToken())
+                    .asClass(TodoOrdersBean.class)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(todoOrdersBean -> {
+                        if (todoOrdersBean.getCode() == 200) {
+                            if (todoOrdersBean.getData() != null && todoOrdersBean.getData().size() > 0) {
+                                boolean canFind = false;
+                                List<TodoOrdersBean.DataBean> dataBeanList = new ArrayList<>();
+
+                                for (TodoOrdersBean.DataBean datum : todoOrdersBean.getData()) {
+                                    if (datum.getLock().getHexUid().equals(result)) {
+                                        canFind = true;
+                                        dataBeanList.add(datum);
+                                    }
+                                }
+                                if (canFind) {
+                                    Intent intent = new Intent(this, TodoOrdersActivity.class);
+                                    Bundle bundle = new Bundle();
+                                    bundle.putSerializable("todo", (Serializable) dataBeanList);
+                                    intent.putExtras(bundle);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Intent intent = new Intent(this, NoTicketActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            } else {
+                                Toast.makeText(BaseApp.getContext(), "无法访问该设备!您可以换个设备试试", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        } else {
+                            Toast.makeText(BaseApp.getContext(), todoOrdersBean.getMsg(), Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }, throwable -> {
+                        Toast.makeText(BaseApp.getContext(), throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                        finish();
+                    });
+            return true;
+        }
     }
 }
